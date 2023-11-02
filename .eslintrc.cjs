0b33c882325130fee72f00d5df1067b4de4eb22e
Lucas Santos Mumbarra <lucassantos.contatoli@gmail.com>
2023-11-02 15:34:50 -0300

    Choise: updated to configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
+    "prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
