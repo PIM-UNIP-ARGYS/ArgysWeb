5e7716e91d470a9eb0fa891afb92718f38927128
Lucas Santos Mumbarra <lucassantosm.2014@gmail.com>
2023-11-09 14:06:48 -0300

    Changed personal data and app footer

--- src/views/pages/personalData/PersonalData.js
+++ src/views/pages/personalData/PersonalData.js
@@ -1,80 +1,70 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import {
   CButton,
   CCard,
   CCardBody,
   CCardGroup,
+  CCardHeader,
   CCol,
   CContainer,
-  CForm,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
   CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
-import logo from '../../../assets/images/logo.png'
+import { cilPlus } from '@coreui/icons'
 
 const PersonalData = () => {
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md="8">
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <img src={logo} alt="Logo Argys Technology" />
-                    <p className="text-medium-emphasis">Preencha seus dados para entrar</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="E-mail" autoComplete="email" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Senha"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CButton color="primary" className="w-100">
-                      Login
-                    </CButton>
-                    <div className="text-center mt-2">
-                      <Link to="/reset-password">Recuperar Senha?</Link>
-                    </div>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5">
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Inscrever-se</h2>
-                    <p>
-                      Você busca eficiência, simplicidade e controle total para o gerenciamento do
-                      seu negócio? Nosso ERP é a resposta. Inscreva-se!
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Cadastre-se!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+    <CContainer>
+      <CRow>
+        <CCol md="12">
+          <CCard className="p-1">
+            <CCardHeader>
+              <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                <CButton color="success" className="me-md-2">
+                  Adicionar <CIcon icon={cilPlus} />
+                </CButton>
+              </div>
+            </CCardHeader>
+            <CCardBody>
+              <CTable responsive bordered>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">Código</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Nome</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">RG</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Nº Carteira Trab.</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">CPF</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">PIS</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Titulo de Eleitor</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Data de Nascimento</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Ações</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  <CTableRow>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                    <CTableDataCell>Cell</CTableDataCell>
+                  </CTableRow>
+                </CTableBody>
+              </CTable>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
   )
 }
 
